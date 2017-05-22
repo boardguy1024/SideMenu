fa860e091ef888992e2defca40a939e40d27a58f
park kyung suk <boardguy1024@gmail.com>
2017-05-22 23:55:25 +0900

    autolayoutでアニメーションを使ってサイドメニュが表示される

--- SideMenu/ViewController.swift
+++ SideMenu/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var menuView: UIView!
+    
+    var isMenuShowing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        leadingConstraint.constant = -160 //-150
+        
+        menuView.layer.shadowOpacity = 0.5
+        menuView.layer.shadowRadius = 6
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openMenu(_ sender: Any) {
+        
+        if isMenuShowing {
+            leadingConstraint.constant = -150
+        } else {
+            leadingConstraint.constant = 0
+            
+            UIView.animate(withDuration: 0.3, animations: { 
+                self.view.layoutIfNeeded()
+            })
+        }
+        isMenuShowing = !isMenuShowing
     }
-
-
 }
 
